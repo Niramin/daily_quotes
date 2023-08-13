524167af7a3770c3159247c39bd37dfb72d97d09
Niramin <shashwatratna@gmail.com>
2023-08-13 12:42:05 +0530

    cloud going left to right smoothly

--- lib/screens/BackgroundSkies.dart
+++ lib/screens/BackgroundSkies.dart
@@ -1,6 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart' as intl;
+import 'dart:math';
+import 'package:rxdart/rxdart.dart';
+
+// State Provider
+class SkyAppState extends ChangeNotifier {
+  final _random = new Random();
+  int next(int min, int max) => min + _random.nextInt(max - min);
+  var dayOfWeek = intl.DateFormat('EEEE').format(DateTime.now());
+  int noOfCloud = 0;
+  bool listenToClock = false;
+  var clock = Stream.periodic(const Duration(seconds: 1));
+  var clockBroadcast = BehaviorSubject();
+  double cloud_width = 50;
+  double cloud_height = 200;
+  double cloud_dist_from_left = 0;
+  double cloud_dist_from_top = 51;
+
+  double screen_max_width = 700;
+
+  double screen_max_height = 900;
+
+  bool isVisible = true;
+  void setMaxXY(double x, double y) {
+    screen_max_height = y;
+    screen_max_width = x;
+  }
+
+  void setCloudPostion(double x, double y) {
+    cloud_dist_from_top = y;
+    cloud_dist_from_left = x;
+  }
+
+  void setCloudRandomSize() {
+    cloud_width = next(50, 300) as double;
+    cloud_height = next(200, 400) as double;
+  }
+
+  void traverseLeft(double distance) {
+    cloud_dist_from_left += distance;
+  }
+
+  void toggleListeningToClock() {
+    listenToClock = !listenToClock;
+  }
+
+  //Constructor
+  SkyAppState() {
+    noOfCloud = next(1, 6);
+    clockBroadcast.listen((value) {
+      if (true) {
+        traverseLeft(5);
+        if (cloud_dist_from_left < 0) {
+          isVisible = true;
+        }
+        if (cloud_dist_from_left > screen_max_width) {
+          isVisible = false;
+        }
+        if (cloud_dist_from_left > screen_max_width + 20) {
+          cloud_dist_from_left = -50;
+        }
+        notifyListeners();
+        print("$cloud_dist_from_left");
+      }
+    });
+    clock.listen((event) => clockBroadcast.add(0));
+    // clock.listen((event) {
+    //   print("hi");
+    // });
+  }
+}
 
 class MorningSky extends StatelessWidget {
   const MorningSky({super.key});
@@ -8,52 +78,56 @@ class MorningSky extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var dayOfWeek = intl.DateFormat('EEEE').format(DateTime.now());
-    return Scaffold(
-      backgroundColor: Colors.blue.shade200,
-      appBar: AppBar(
-        title: Center(
-            child: Text(
-          dayOfWeek,
-          style: const TextStyle(
-            fontFamily: 'MedievalSharp',
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        )),
-      ),
-      body: Stack(
-        children: [
-          Clouds(),
-          Center(
-            child: Text(
-              "Sample Text\nSample Text\nSample Text\nSample Text\nSample Text\n",
+    return ChangeNotifierProvider(
+      create: (context) => SkyAppState(),
+      child: Scaffold(
+        backgroundColor: Colors.blue.shade200,
+        appBar: AppBar(
+          backgroundColor: Colors.grey.shade400,
+          title: Center(
+              child: Text(
+            dayOfWeek,
+            style: const TextStyle(
+              fontFamily: 'MedievalSharp',
+              fontWeight: FontWeight.bold,
+              color: Colors.limeAccent,
             ),
-          ),
-        ],
+          )),
+        ),
+        body: Stack(
+          children: [
+            Clouds(),
+            Center(
+              child: Text(
+                "Sample Text\nSample Text\nSample Text\nSample Text\nSample Text\n",
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () => {},
+              tooltip: 'Like',
+              backgroundColor: Colors.grey.shade400,
+              foregroundColor: Colors.limeAccent,
+              child: const Icon(Icons.favorite),
+            ),
+            const SizedBox(
+              width: 70,
+            ),
+            FloatingActionButton(
+              onPressed: () => {},
+              tooltip: 'Shuffle',
+              backgroundColor: Colors.grey.shade400,
+              foregroundColor: Colors.limeAccent,
+              child: const Icon(Icons.shuffle),
+            ),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: () => {},
-            tooltip: 'Like',
-            backgroundColor: Colors.brown.shade400,
-            foregroundColor: Colors.limeAccent,
-            child: const Icon(Icons.favorite),
-          ),
-          const SizedBox(
-            width: 70,
-          ),
-          FloatingActionButton(
-            onPressed: () => {},
-            tooltip: 'Shuffle',
-            backgroundColor: Colors.brown.shade400,
-            foregroundColor: Colors.limeAccent,
-            child: const Icon(Icons.shuffle),
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
@@ -68,22 +142,21 @@ class Clouds extends StatefulWidget {
 }
 
 class _CloudsState extends State<Clouds> {
-  double cur_x = 0.0;
-  double cur_y = 51.0;
   @override
   Widget build(BuildContext context) {
-    return AnimatedPositioned(
-      duration: const Duration(seconds: 2),
-      width: 50.0,
-      height: 200.0,
-      top: 51,
-      left: cur_x,
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            cur_x += 10.0;
-          });
-        },
+    var appState = context.watch<SkyAppState>();
+    //initialise cloud
+    appState.setMaxXY(
+        MediaQuery.of(context).size.width, MediaQuery.of(context).size.height);
+
+    return Visibility(
+      visible: appState.isVisible,
+      child: AnimatedPositioned(
+        duration: const Duration(seconds: 2),
+        width: appState.cloud_width,
+        height: appState.cloud_width,
+        top: appState.cloud_dist_from_top,
+        left: appState.cloud_dist_from_left,
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
